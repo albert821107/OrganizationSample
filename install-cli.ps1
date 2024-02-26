8876bc4f388fa11c8c95f3b55d818c3df83dcdf8
有臨 陳 <albert1107@ptc-nec.com.tw>
2024-02-26 09:51:05 +0800

    先加入環境檔

---
+++ install-cli.ps1
@@ -0,0 +1,20 @@
+﻿# install scoope
+Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+
+# install go-task
+scoop bucket add extras
+scoop install task
+Scoop search task
+
+# install dotnet-ef 
+dotnet tool update --global dotnet-ef
+
+# insatll choco
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# insatll python
+choco install python -y
+
+# install mssql-scripter
+pip install mssql-scripter
