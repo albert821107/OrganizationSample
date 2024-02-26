4aa8aa81fcaf818ad58aaed9af6dd73f9407002f
Flash <albert821107@gmail.com>
2024-02-26 19:33:48 +0800

    新增環境資料表

---
+++ PolicyHolder.sql
@@ -0,0 +1,22 @@
+USE [sample]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[PolicyHolder](
+	[code] [varchar](10) NOT NULL,
+	[name] [nvarchar](max) NOT NULL,
+	[registration_date] [date] NOT NULL,
+	[introducer_code] [varchar](10) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[code] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
---
+++ PolicyHolder_BinaryTree.sql
@@ -0,0 +1,34 @@
+USE [sample]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[PolicyHolder_BinaryTree](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[policyholder_code] [varchar](10) NOT NULL,
+	[left_child_id] [int] NULL,
+	[right_child_id] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[PolicyHolder_BinaryTree]  WITH CHECK ADD FOREIGN KEY([left_child_id])
+REFERENCES [dbo].[PolicyHolder_BinaryTree] ([id])
+GO
+
+ALTER TABLE [dbo].[PolicyHolder_BinaryTree]  WITH CHECK ADD FOREIGN KEY([policyholder_code])
+REFERENCES [dbo].[PolicyHolder] ([code])
+GO
+
+ALTER TABLE [dbo].[PolicyHolder_BinaryTree]  WITH CHECK ADD FOREIGN KEY([right_child_id])
+REFERENCES [dbo].[PolicyHolder_BinaryTree] ([id])
+GO
+
+
